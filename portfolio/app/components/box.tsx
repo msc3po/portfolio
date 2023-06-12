559eb11cf209570625d08c7e3c2865fb6f92b7da
Manel Soler <manelsoler@gmail.com>
2023-06-12 21:13:05 +0200

    more home styling and refactor layout and home code

--- portfolio/app/components/box.tsx
+++ portfolio/app/components/box.tsx
@@ -7,13 +7,12 @@ type BoxProps = {
 export default function Box({title, content}: BoxProps) {
   return (
     <div className="left-0 top-0 flex h-full w-full items-center justify-center py-10">
-    <div className="glass rounded-2xl border-solid border-2 border-gray-200 p-4 w-64 h-64 flex items-center justify-center">
-      <div>
-        <h1 className="mb-4 text-3xl font-extrabold text-center">{title}</h1>
-        <p className="text-center">{content}</p>
+      <div className="glass rounded-2xl border-solid border-2 border-gray-200 p-4 w-64 h-64 flex items-center justify-center transition-all duration-200 ease-in-out hover:shadow-lg hover:bg-blue-100">
+        <div>
+          <h1 className="mb-4 text-3xl font-extrabold text-center">{title}</h1>
+          <p className="text-center">{content}</p>
+        </div>
       </div>
     </div>
-  </div>
-)
-  
+  )
 }
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,23 +1,29 @@
 import Image from 'next/image'
 import Box from './components/box'
-
+import Link from 'next/link'
 
 export default function Home() {
+  const boxes = [
+    { title: 'About', content: 'About me', href: '/about' },
+    { title: 'Portfolio', content: 'My Portfolio', href: '/portfolio' },
+    { title: 'Blog', content: 'My Blog', href: '/blog' },
+    { title: 'Contact', content: 'Contact me', href: '/contact' },
+  ]
+
   return (
-    
-    <div className="bg-gradient-to-r from-slate-50 to-slate-100 h-screen">
+    <div className="bg-gradient-to-r from-slate-50 to-slate-100 min-h-screen">
       <div className="h-max-full text-center">
-
-      <h1>Welcome to Manel Soler Portfolio</h1>
-        <div className=" items-center grid grid-rows-{3} gap-4 ">
-          <Box title='About' content='About me' />
-          <Box title='Contact' content='Contact me' />
-          <Box title='Portfolio' content='My Portfolio' />
-          <Box title='Blog' content='My Blog' />
-      
-        </div>
-        </div>
-
-  </div>
+        <h1 className="p-10 py-20">Welcome to Manel Soler Portfolio</h1>
+        <nav className="items-center grid grid-cols-2 gap-4 mx-auto w-3/4">
+          {boxes.map((box, index) => (
+            <Link href={box.href}>
+              
+                <Box key={index} title={box.title} content={box.content} />
+              
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </div>
   )
 }
