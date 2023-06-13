e47d8db806e560e4262cfad9c4d532eee8e7325b
Manel Soler <manelsoler@gmail.com>
2023-06-13 19:17:50 +0200

    solving format home boxes

--- portfolio/app/components/box.tsx
+++ portfolio/app/components/box.tsx
@@ -1,23 +1,30 @@
-//create a box comoponent and export it to the page
 type BoxProps = {
   title: string
   content: string
   image: string
   size?: string
-  shape?: 'circle' | 'square'
+  shape?: 'circle' | 'square' | 'rectangle'
+  textCentered?: boolean
 }
 
 export default function Box({
   title,
   content,
   image,
-  shape = 'rectangle',
+  shape = 'square',
+  textCentered = false,
 }: BoxProps) {
   return (
     <div className="flex justify-center py-10">
       <div
-        className={`glass rounded-2xl border-solid border-2 border-gray-200 p-4 flex justify-center transition-all duration-200 ease-in-out hover:shadow-lg hover:bg-blue-100 ${
-          shape === 'circle' ? 'rounded-full w-64 h-64' : 'w-full h-full'
+        className={`glass rounded-2xl border-solid border-2 border-gray-200 p-4 ${
+          textCentered ? 'flex items-center justify-center' : ''
+        } transition-all duration-200 ease-in-out hover:shadow-lg hover:bg-blue-100 ${
+          shape === 'circle'
+            ? 'rounded-full w-64 h-64'
+            : shape === 'square'
+            ? 'aspect-[1/1] w-64 h-64'
+            : 'w-full h-full'
         }`}
         style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover' }}
       >
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="bg-gradient-to-r from-slate-50 to-slate-100 min-h-screen">
       <div className="h-max-full text-center">
         <h1 className="p-10 py-20">Welcome to Manel Soler Portfolio</h1>
-        <nav className="items-center grid grid-cols-2 gap-4 mx-auto w-3/4">
+        <nav className="items-center grid grid-cols-2 gap-4 mx-auto w-3/4 ">
           {boxes.map((box, index) => (
             <Link href={box.href}>
               <Box
@@ -22,6 +22,7 @@ export default function Home() {
                 title={box.title}
                 content={box.content}
                 image={''}
+                textCentered={true}
               />
             </Link>
           ))}
