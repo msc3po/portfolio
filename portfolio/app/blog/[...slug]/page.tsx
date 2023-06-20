200b25830290295db6391f283d72cc909b745151
Manel Soler <manelsoler@gmail.com>
2023-06-20 15:03:47 +0200

    styling post

--- portfolio/app/blog/[...slug]/page.tsx
+++ portfolio/app/blog/[...slug]/page.tsx
@@ -1,6 +1,7 @@
-
-export default function BlogPost(){
+export default function BlogPost() {
   return (
-  <div>Post</div>
+    <div className=" flex items-center justify-center h-screen bg-gradient-to-r from-slate-50 to-slate-100 min-h-screen ">
+      Post
+    </div>
   )
 }
